d86e8b8cf4c988d5adc29459f2ac4b79190d3ad4
themanojshukla <themanojshukla@gmail.com>
2017-11-08 20:14:27 +0530

    Added Edit,delete and update feature

--- src/app/employee/components/employeeDetails.component.ts
+++ src/app/employee/components/employeeDetails.component.ts
@@ -1,27 +1,84 @@
-import { Component, NgModule, Input } from "@angular/core";
+import { Component, NgModule, Input, Output, OnInit, OnChanges, EventEmitter } from "@angular/core";
 import { NgIf } from '@angular/common';
 import {
-    ReactiveFormsModule,
-    FormsModule,
     FormGroup,
     FormControl,
-    Validators,
-    FormBuilder
+    Validators
 } from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 
+
 @Component({
     selector: 'employee-details',
     templateUrl: './../templates/employeeDetails.component.html',
+    styleUrls: ['./../css/employeeDetails.component.css']
 })
 
 export class EmployeeDetailsComponent {
-    
-   @Input() employee : any ;
- 
-  onSubmit() {
-      console.log("Form Submitted!");
+   @Input() employee: any ;
+   @Input() employees: any ;
+   
+   @Output() notify: EventEmitter<any> = new EventEmitter<any>();
+   form;
+   payLoad = null;
+   updateForm;
+   updateGroup:any = {};
+  
+
+    ngOnChanges(): void{
+        if(this.employee){
+        this.updateGroup.id = new FormControl(this.employee.id,Validators.required);
+        this.updateGroup.firstName =  new FormControl(this.employee.firstName, Validators.required);
+        this.updateGroup.middleName = new FormControl(this.employee.middleName, Validators.required);
+        this.updateGroup.lastName = new FormControl(this.employee.lastName, Validators.required);
+        this.updateGroup.dateOfBirth = new FormControl(this.employee.dateOfBirth,Validators.required);
+        this.updateForm = new FormGroup(this.updateGroup);
+        }
     }
+
+
+   constructor() {
+       let group:any = {};
+       group.firstName =  new FormControl('', Validators.required);
+       group.middleName = new FormControl('', Validators.required);
+       group.lastName = new FormControl('', Validators.required);
+       group.dateOfBirth = new FormControl('',Validators.required);
+       this.form = new FormGroup(group);    
+   }
+
+   onSubmit() {
+       this.payLoad = this.form.value;
+       new EmployeeDetailsComponent();
+       var temp : any[]= this.employees;
+       this.payLoad.id = temp.length+1;
+       temp.push(this.payLoad );
+       this.notify.emit(temp);
+       
+   }
+
+   deleteEmp(index){
+    var temp : any[]= this.employees;
+    temp.splice(index, 1);
+    console.log(temp);
+    this.notify.emit(temp);
+   }
+
+   onUpdate() {
+       this.payLoad = this.updateForm.value;
+       
+       var temp : any[]= this.employees;
+       var objIndex = temp.findIndex((obj => obj.id == this.payLoad.id ));
+       console.log(objIndex);
+       temp[objIndex].firstName = this.payLoad.firstName;
+       temp[objIndex].middleName = this.payLoad.middleName;
+       temp[objIndex].lastName = this.payLoad.lastName;
+       temp[objIndex].dateOfBirth = this.payLoad.dateOfBirth;
+
+       this.notify.emit(temp);
+   }
+
+
+
 }
