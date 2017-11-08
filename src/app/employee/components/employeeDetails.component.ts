b98518b2d989b1f172187a6dedfe30e1fc3d0b57
themanojshukla <themanojshukla@gmail.com>
2017-11-08 13:03:08 +0530

    modified employeeDetails Component

--- src/app/employee/components/employeeDetails.component.ts
+++ src/app/employee/components/employeeDetails.component.ts
@@ -17,58 +17,11 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
     templateUrl: './../templates/employeeDetails.component.html',
 })
 
-export class EmployeeDetailsComponent implements OnInit {
+export class EmployeeDetailsComponent {
     
    @Input() employee : any ;
-   langs: string[] = [
-    'English',
-    'French',
-    'German',
-  ];
-  myform: FormGroup;
-  firstName: FormControl;
-  lastName: FormControl;
-  email: FormControl;
-  password: FormControl;
-  language: FormControl;
-
-
-  ngOnInit() {
-    this.createFormControls();
-    this.createForm();
-  }
-
-  createFormControls() {
-    this.firstName = new FormControl('', Validators.required);
-    this.lastName = new FormControl('', Validators.required);
-    this.email = new FormControl('', [
-      Validators.required,
-      Validators.pattern("[^ @]*@[^ @]*")
-    ]);
-    this.password = new FormControl('', [
-      Validators.required,
-      Validators.minLength(8)
-    ]);
-    this.language = new FormControl('');
-  }
-
-  createForm() {
-    this.myform = new FormGroup({
-      name: new FormGroup({
-        firstName: this.firstName,
-        lastName: this.lastName,
-      }),
-      email: this.email,
-      password: this.password,
-      language: this.language
-    });
-  }
-
+ 
   onSubmit() {
-    if (this.myform.valid) {
       console.log("Form Submitted!");
-      this.myform.reset();
     }
-  }
-  
 }
